ca8c120722f86cf36c9fdedb767e40b316804411
Nicolas Jeanselme <41996571+njeanselme@users.noreply.github.com>
2021-03-09 13:39:16 +0100

    Update botdc-endpoints-to-endpoint_groups.py
    added paging support for larger number of endpoint. Tested with 250.000 endpoints

--- botdc-endpoints-to-endpoint_groups.py
+++ botdc-endpoints-to-endpoint_groups.py
@@ -23,27 +23,38 @@ def build_mapping_table(file):
 
 
 def get_endpoints_to_map(hostname, api_token, table):
-    try:
-        response = requests.get("https://{}/api/atcep/v1/roaming_devices".format(hostname), headers=headers,
-                                verify=True, timeout=(30, 30))
+    offset = 0
+    limit = 10000
+    r_json= []
+    more_results= True
+
+    while more_results:
+        url = "https://{}/api/atcep/v1/roaming_devices?_offset={}&_limit={}".format(hostname, offset, limit)
         try:
-            r_json = response.json()
+            response = requests.get(url, headers=headers, verify=True, timeout=(30, 30))
+        except:
+            raise Exception("Unable to perform polling from url:{}, status code:{}".format(url, response.status_code))
+        try:
+            if len(response.json()["results"]):
+                r_json += response.json()["results"]
+            else:
+                more_results = False
         except:
             raise Exception("Response is not valid json. Response:{}".format(response.text))
-    except:
-        raise Exception("Unable to perform polling from url:{}, status code:{}".format(url, response.status_code))
+
+        offset += limit
+        print("Loading enpdoints from csp: {}".format(offset))
 
     endpoints_to_map = {}
     for row in table.rows:
         endpoints_to_map[row['group']] = []
 
     print('Endpoints to remap:')
-    for result in r_json["results"]:
+    for result in r_json:
         for row in table.rows:
             if re.match("^{}.*".format(row["short_name_prefix"]), result["name"], re.IGNORECASE):
                 if not re.match("^{}$".format(row["group"]), result["group_name"], re.IGNORECASE):
-                    print("fqdn: {}, current_group: {}, future_group {}".format(result["name"], result["group_name"],
-                                                                                row["group"]))
+                    print("fqdn: {}, current_group: {}, future_group {}".format(result["name"], result["group_name"], row["group"]))
                     endpoints_to_map[row["group"]].append(result["client_id"])
     print('---------------------------------------')
     return endpoints_to_map
@@ -68,23 +79,21 @@ def map_endpoints_to_groups(hostname, api_token, endpoints_to_map, endpoint_grou
 
             if group == "disabled" or group == "deleted":
                 print("Disabling {}".format(endpoints_to_map[group]))
+                # administrative_status: "DISABLED"
+                # client_ids: ["id1","id2"]
                 url = "https://{}/api/atcep/v1/roaming_devices".format(hostname)
                 data = {}
                 data["administrative_status"] = "DISABLED"
                 data["client_ids"] = endpoints_to_map[group]
 
                 try:
-                    response = requests.put(url,
-                                            headers=headers,
-                                            data=json.dumps(data, indent=4),
-                                            verify=True, timeout=(30, 30))
+                    response = requests.put(url, headers=headers, data=json.dumps(data, indent=4), verify=True, timeout=(30, 30))
                     try:
                         print(response.json())
                     except:
                         raise Exception("Response is not valid json. Response:{}".format(response.text))
                 except:
-                    raise Exception("Unable to perform polling from url:{}, status code:{}".format(url,
-                                                                                                   response.status_code))
+                    raise Exception("Unable to perform polling from url:{}, status code:{}".format(url, response.status_code))
 
                 if group == "deleted":
                     print("Deleting {}".format(endpoints_to_map[group]))
@@ -94,18 +103,13 @@ def map_endpoints_to_groups(hostname, api_token, endpoints_to_map, endpoint_grou
                     data["administrative_status"] = "DELETED"
                     data["client_ids"] = endpoints_to_map[group]
                     try:
-                        response = requests.put(url,
-                                                headers=headers,
-                                                data=json.dumps(data, indent=4),
-                                                verify=True, timeout=(30, 30))
+                        response = requests.put(url, headers=headers, data=json.dumps(data, indent=4), verify=True, timeout=(30, 30))
                         try:
                             print(response.json())
                         except:
                             raise Exception("Response is not valid json. Response:{}".format(response.text))
                     except:
-                        raise Exception("Unable to perform polling from url:{}, status code:{}".format(url,
-                                                                                                       response.status_code))
-
+                        raise Exception("Unable to perform polling from url:{}, status code:{}".format(url, response.status_code))
 
             else:
                 print("Adding {} to group: {}".format(endpoints_to_map[group], group))
@@ -116,18 +120,13 @@ def map_endpoints_to_groups(hostname, api_token, endpoints_to_map, endpoint_grou
                         print("Group after: \n{}".format(json.dumps(endpoint_group, indent=4, sort_keys=True)))
                         url= "https://{}/api/atcep/v1/roaming_device_groups/{}".format(hostname,endpoint_group['id'])
                         try:
-                            response = requests.put(url,
-                                                    headers=headers,
-                                                    data=json.dumps(endpoint_group, indent=4, sort_keys=True),
-                                                    verify=True, timeout=(30, 30))
+                            response = requests.put(url, headers=headers, data=json.dumps(endpoint_group, indent=4, sort_keys=True), verify=True, timeout=(30, 30))
                             try:
                                 print(response.json())
                             except:
                                 raise Exception("Response is not valid json. Response:{}".format(response.text))
                         except:
-                            raise Exception("Unable to perform polling from url:{}, status code:{}".format(url,
-                                                                                                           response.status_code))
-
+                            raise Exception("Unable to perform polling from url:{}, status code:{}".format(url, response.status_code))
 
 mapping_table = build_mapping_table(file)
 endpoints_to_map = get_endpoints_to_map(hostname, api_token, mapping_table)
